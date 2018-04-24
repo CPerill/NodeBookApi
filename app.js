963b8ad83d793ba8b855e6d6511024194a5555fb
Connor Perill <connorjperill@gmail.com>
2018-04-25 00:04:02 +0100

    Updated code

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var Book = require('./models/bookModel');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+
 
 var app = express();
+
 var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+
+bookRouter.route('/books')
+    .get(function(req, res) {
+        Book.find(function(err, books) {
+            if(err) {
+                console.log(err);
+            } else {
+                res.json(books);
+            }
+        });
+    });
+
+app.use('/api', bookRouter);
 
 app.get('/', function(req, res) {
     res.send('Welcome to Connor\'s API');
